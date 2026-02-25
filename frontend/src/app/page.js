0ff11e2ffc7cf92043775c8d223757f4efd67ba6
thanuja-Foreforms <thanuja.rajawardana@foreforms.se>
2026-02-25 19:33:31 +0530

    feat: Initialize Next.js frontend project with basic structure, dependencies, and configuration files.

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex text-center'>page
+
+      <h1 className='flex text-center text-orange-500'>Hello Google</h1>
+    </div>
+  )
+}
