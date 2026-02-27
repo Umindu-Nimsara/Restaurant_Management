dea64d3940dec4588b315266915ea1dc207090e9
thanuja-Foreforms <thanuja.rajawardana@foreforms.se>
2026-02-27 21:54:17 +0530

    feat: Set up initial frontend application with admin module and core UI components.

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import AdminHomePage from "./modules/admin/page";
 
 function page() {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Home</h1>
+      <AdminHomePage />
     </div>
   )
 }
---
+++ frontend/components/ui/module.js
@@ -0,0 +1,12 @@
+// components/ui/modules.tsx
+
+import { icons } from "./icon"
+
+export const modules = [
+    { name: "Menu Items Management", route: "/modules/menu-items-management", icon: icons.HandPlatter },
+    { name: "Inventory Management", route: "/modules/inventory-management", icon: icons.ShoppingCart },
+    { name: "Order Management", route: "/modules/order-management", icon: icons.BadgeDollarSign },
+    { name: "Supplier Management", route: "/modules/supplier-management", icon: icons.StoreIcon },
+    { name: "Table Reservation", route: "/modules/table-reservation", icon: icons.DoorOpenIcon },
+    { name: "User Account Management", route: "/modules/user-account-management", icon: icons.UserPen },
+]
